5f2f1d79e5be46f0bb24344cc18bdaa3d4d54a2d
Michael Walczyk <michael.walczyk@obscuradigital.com>
2017-10-30 16:22:29 -0800

    Removes target directory.

--- target/debug/librust-test.d
+++
@@ -1,1 +0,0 @@
-C:\Users\michael.walczyk\Desktop\rust-test\target\debug\librust-test.rmeta: C:\Users\michael.walczyk\Desktop\rust-test\src\main.rs
--- target/debug/rust-test.d
+++
@@ -1,1 +0,0 @@
-C:\Users\michael.walczyk\Desktop\rust-test\target\debug\rust-test.exe: C:\Users\michael.walczyk\Desktop\rust-test\src\main.rs
